e78895ac830e3767c636f1f6d6fa0c72500063c3
Adam Lewis <adamlewisp511@gmail.com>
2018-06-16 01:41:33 -0400

    "added comment"

--- js/SWreg.js
+++ js/SWreg.js
@@ -1,3 +1,5 @@
+//Registering serviceWorker
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
