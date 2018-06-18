6969e89f7c5af7ad3bb67ed8c554a67518bbabb3
Adam Lewis <adamlewisp511@gmail.com>
2018-06-18 18:09:00 -0400

    "added aria-roles to index"

--- sw.js
+++ sw.js
@@ -1,65 +1,47 @@
-//Cache created
-const cache_mws = "restaurant";
-const files_Cache = [
-  '/',
-  '/css/styles.css',
-  '/data/restaurant.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/resturant_info.js',
-  '/js/SWreg.js',
-  '/index.html',
-  '/package-lock.json',
-  '/restaurant.html',
-  '/sw.js'
-  //'https://normalize-css.googlecode.com/svn/trunk/normalize.css'
-  ];
-
-
-//Setting up listener for serviceWorker event "install"
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
-  console.log("[Servicework] Install");
+}); var CACHE_NAME = 'restaurant-cache';
+var urlsToCache = [
+  '/',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open(cache_mws).then(function(cache) {
-      console.log("[ServiceWorker] Caching app shell");
-      return cache.addAll(files_Cache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-//Setting up listener for serviceWorker event "activate"
-self.addEventListener("activate", function(event) {
-  console.log("[Servicework] Activate");
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cache_mws) {
-          console.log("[ServiceWorker] Removing old cache shell", key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
 });
 
-//Setting up listener for serviceWorker event "fetch"
-self.addEventListener("fetch", (event) => {
-  console.log("[ServiceWorker] Fetch");
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
     })
+    .catch(err => console.log(err, event.request))
   );
-
 });
