e0c7b62733aa4e1cecb900a9bdadb61bd99fda3c
Adam Lewis <adamlewisp511@gmail.com>
2018-06-26 22:56:54 -0400

    "Fixed serviceWorker"

--- sw.js
+++ sw.js
@@ -1,47 +1,46 @@
-self.addEventListener('install', function (event) {
-  // Perform install steps
-}); var CACHE_NAME = 'restaurant-cache';
-var urlsToCache = [
-  '/',
-  './index.html',
-  './restaurant.html',
-  './css/styles.css',
-  './js/dbhelper.js',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './data/restaurants.json',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
-];
 
-self.addEventListener('install', function (event) {
+
+//Setting up listener for serviceWorker event "install"
+self.addEventListener("install", function(event) {
   // Perform install steps
+  console.log("[Servicework] Install");
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('restaurant').then(function(cache) {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll([
+        '/',
+        'css/styles.css',
+        'data/restaurants.json',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        'index.html',
+        'restaurant.html',
+        'sw.js',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg'
+        //'https://normalize-css.googlecode.com/svn/trunk/normalize.css'
+        ])
+    })
   );
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
 
-self.addEventListener('fetch', event => {
+
+//Setting up listener for serviceWorker event "fetch"
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
-    .catch(err => console.log(err, event.request))
   );
 });
